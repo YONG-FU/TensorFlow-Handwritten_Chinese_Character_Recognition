7b9d41d21de10214a820d58724b1b7769f114c4f
yong-fu <15026929959@139.com>
2018-01-07 18:18:08 +0800

    Project files updated

--- keras-train.py
+++ keras-train.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import os
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Input, Dense, Dropout, Convolution2D, MaxPooling2D, Flatten
+from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from keras.models import Model, load_model
 
 data_dir = '../Datasets'
@@ -9,61 +9,56 @@ train_data_dir = os.path.join(data_dir, 'train')
 test_data_dir = os.path.join(data_dir, 'test')
 
 # dimensions of our images.
-img_width, img_height = 64, 64
 image_classes = 3755
-nb_validation_samples = 800
-nb_samples_per_epoch = 2000
-nb_nb_epoch = 20000;
+image_width, image_height = 64, 64
 
 
 def train(model):
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        rotation_range=0,
-        width_shift_range=0.1,
-        height_shift_range=0.1
-    )
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    train_data_generator = ImageDataGenerator(rescale=1./255)
+    test_data_generator = ImageDataGenerator(rescale=1./255)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=1024,
+    train_generator = train_data_generator.flow_from_directory(
+        directory=train_data_dir,
+        target_size=(image_width, image_height),
+        batch_size=600,
         color_mode="grayscale",
         class_mode='categorical')
 
-    validation_generator = test_datagen.flow_from_directory(
-        test_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=1024,
+    validation_generator = test_data_generator.flow_from_directory(
+        directory=test_data_dir,
+        target_size=(image_width, image_height),
+        batch_size=600,
         color_mode="grayscale",
         class_mode='categorical')
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='nadam',
+        metrics=['accuracy'])
 
-    model.fit_generator(train_generator,
-                        samples_per_epoch=nb_samples_per_epoch,
-                        nb_epoch=nb_nb_epoch,
-                        validation_data=validation_generator,
-                        nb_val_samples=nb_validation_samples)
+    model.fit_generator(
+        generator=train_generator,
+        steps_per_epoch=1500,
+        epochs=100,
+        verbose=1,
+        validation_data=validation_generator,
+        validation_steps=400)
 
 
 def build_model(include_top=True, input_shape=(64, 64, 1), classes=image_classes):
     img_input = Input(shape=input_shape)
-    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same', name='block1_conv1')(img_input)
-    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
-    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block2_conv1')(x)
-    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    x = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block1_pool')(x)
+    x = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block2_pool')(x)
 
     if include_top:
         x = Flatten(name='flatten')(x)
         x = Dropout(0.05)(x)
         x = Dense(1024, activation='relu', name='fc2')(x)
-        x = Dense(image_classes, activation='softmax', name='predictions')(x)
+        x = Dense(classes, activation='softmax', name='predictions')(x)
 
         model = Model(img_input, x, name='model')
         return model
